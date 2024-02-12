6fec74f2aef04d940f5fedfceba1837a427636dd
Mateo Gomez <mateogomezmolina@gmail.com>
2024-02-12 16:26:44 -0500

    feat: add header

---
+++ src/constants/routes.ts
@@ -0,0 +1,18 @@
+export const ROUTES = [
+	{
+		name: "inicio",
+		link: "#home",
+	},
+	{
+		name: "Experiencia",
+		link: "#experience",
+	},
+	{
+		name: "Proyectos",
+		link: "#projects",
+	},
+	{
+		name: "Sobre mi",
+		link: "#about",
+	},
+];
